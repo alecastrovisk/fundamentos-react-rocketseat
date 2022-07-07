e6ff3f58e7a790404809a198ef081171132ef6f4
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-07-07 10:28:02 -0300

    feat:  Adding props to the post

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,57 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alecastrovisk.png',
+      name: 'Alexandre Castro',
+      role: 'Front-end developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de desistir da matéria da mônica!🚀' },
+      { type: 'link', content: '👉leohas/dev' },
+    ],
+    publishedAt: new Date('2022-07-07 08:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/leohas.png',
+      name: 'Leonardo Henrique',
+      role: 'Front-end developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de desistir da matéria da mônica!🚀' },
+      { type: 'link', content: '👉leohas/dev' },
+    ],
+    publishedAt: new Date('2022-07-08 10:00:00')
+  }
+];
+
 function App() {
   return (
     <div>
       <Header />
-      
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Alexandre Castro"
-            content="blablabla"
-          />
-          <Post 
-            author="Jalin Habey"
-            content="Enxadrista indiano"
-          />
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
